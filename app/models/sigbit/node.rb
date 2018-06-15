e96070977d2ff0f6d04b4b53da92bc0a6aa43449
Johan André <johan@significantbit.se>
2018-06-15 23:32:03 +0200

    Using node concerns

--- app/models/sigbit/node.rb
+++ app/models/sigbit/node.rb
@@ -1,4 +1,15 @@
+require "concerns/sigbit/node_methods/relations"
+
 module Sigbit
   class Node < ApplicationRecord
+    extend Mobility
+    include Sigbit::NodeMethods::Tree
+    include Sigbit::NodeMethods::Relations
+
+    translates :menu_title, :url, fallbacks: { en: :sv }, locale_accessors: true
+
+    def contentable
+      page_type.contentable
+    end
   end
 end
