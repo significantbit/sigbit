f098f2b3f7004540d25c9899a8753a3125130120
Johan André <johan@significantbit.se>
2018-06-19 22:21:30 +0200

    Fixing generator

--- lib/generators/content_type/content_type_generator.rb
+++ lib/generators/content_type/content_type_generator.rb
@@ -5,13 +5,14 @@ class ContentTypeGenerator < Rails::Generators::NamedBase
 
   def create_model
     generate "model ContentType::#{class_name} #{fields_as_string}"
-    inject_into_file "#{Rails.root}/app/models/content_type/#{name.downcase}.rb", "  extend Mobility\n  include Contentable\n", after: "class ContentType::#{class_name} < ApplicationRecord\n"
-    inject_into_file "#{Rails.root}/app/models/content_type/#{name.downcase}.rb", "\n  translates :title, fallbacks: { en: :sv }, locale_accessors: true\n", after: "include Contentable\n"
+    inject_into_file "#{Rails.root}/app/models/content_type/#{name.downcase}.rb", "require \"mobility\"\n", before: "class ContentType::#{class_name} < ApplicationRecord\n"
+    inject_into_file "#{Rails.root}/app/models/content_type/#{name.downcase}.rb", "  extend Mobility\n  include Sigbit::Contentable\n", after: "class ContentType::#{class_name} < ApplicationRecord\n"
+    inject_into_file "#{Rails.root}/app/models/content_type/#{name.downcase}.rb", "\n  translates :title, fallbacks: { en: :sv }, locale_accessors: true\n", after: "include Sigbit::Contentable\n"
     inject_into_file Dir.glob("db/migrate/*create_content_type_*").sort.last, "      t.jsonb :translations, default: {}\n", before: "      t.timestamps\n"
   end
 
   def create_form
-    template "form.html.erb", File.join("app/views/admin/content_type", "_#{name.downcase}.html.erb")
+    template "form.html.erb", File.join("app/views/sigbit/content_type", "_#{name.downcase}.html.erb")
   end
 
   def create_frontend
