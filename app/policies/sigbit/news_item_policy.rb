feb683784b40fb7872461038a159e41e1edd30a9
Oscar Nordin <oscar@significantbit.se>
2018-11-07 13:52:48 +0100

    generated pundit policy for news_item

---
+++ app/policies/sigbit/news_item_policy.rb
@@ -0,0 +1,33 @@
+module Sigbit
+  class NewsItemPolicy < ApplicationPolicy
+    def index?
+      user.has_role?(:admin)
+    end
+
+    def new?
+      create?
+    end
+
+    def create?
+      user.has_role?(:admin)
+    end
+
+    def edit?
+      update?
+    end
+
+    def update?
+      user.has_role?(:admin)
+    end
+
+    def destroy?
+      user.has_role?(:admin)
+    end
+
+    class Scope < Scope
+      def resolve
+        scope.all
+      end
+    end
+  end
+end
