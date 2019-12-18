5f6b7b8def7a2910f92eea0e8e8bbc0872aae032
Johan André <johan@significantbit.se>
2019-12-18 14:05:18 +0100

    Default admin role for users

--- app/models/sigbit/user.rb
+++ app/models/sigbit/user.rb
@@ -3,9 +3,16 @@ module Sigbit
     rolify role_cname: "Sigbit::Role"
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable and :omniauthable
+
+    after_create :add_admin_roles
+
     devise :database_authenticatable,
            :recoverable, :rememberable, :trackable, :validatable
 
     has_attachment :image
+
+    def add_admin_roles
+      add_role :admin
+    end
   end
 end
